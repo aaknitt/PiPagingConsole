0e78b299df978cb5601a0f8ddd751505e49eb229
aaknitt <andyknitt@gmail.com>
2022-01-09 21:26:09 -0600

    add pyinstaller build scripts

---
+++ build.bat
@@ -0,0 +1,2 @@
+pyinstaller pipagingconsole.py --clean --onefile --noconsole --name encoder
+REM pyinstaller encoder.py --clean --onefile --noconsole --icon encoder.ico --name encoder
---
+++ build.sh
@@ -0,0 +1,1 @@
+/home/pi/.local/bin/pyinstaller pipagingconsole.py --clean --onefile --noconsole --name encoder
